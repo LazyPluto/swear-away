e2f03bc1bf3a5c8f779e8e63994bc6cd1cac0a9c
LazyPluto <74699152+LazyPluto@users.noreply.github.com>
2025-03-22 00:48:18 +0530

    Added swear-words.txt

--- main.py
+++ main.py
@@ -11,21 +11,31 @@ intents.guilds = True
 intents.members = True
 allowed_mentions = AllowedMentions(users=True)
 
+with open("swear-words.txt") as f:
+	swear_words = [word.strip() for word in f.readlines()]
+
 class SwearAwayBot(discord.Client):
 	async def on_ready(self):
 		print(f'Logged in as {bot.user.name}')
 	
 	async def on_message(self, message: discord.Message):
-		# Prevent the bot from responding to its own messages
 		if message.author == bot.user:
 			return
 
-		if "fuck" in message.content.lower():
+		if should_censor(message.content):
 			await message.delete()
 			await message.channel.send(prettify_message(message.author.mention, message.content.replace("fuck", "flower")), allowed_mentions=allowed_mentions)
 
 def prettify_message(author_name: str, message: str):
 	return f"{author_name}:\n\"{message}\""
 
+def should_censor(message: str):
+	message = message.lower()
+	for word in swear_words:
+		if word in message:
+			return True
+	return False
+
+print(f"Censoring words: {swear_words}")
 bot = SwearAwayBot(intents=intents)
 bot.run(env.BOT_TOKEN)
