dc1b8d75f610630b417668b08274a9f6b3798618
LazyPluto <74699152+LazyPluto@users.noreply.github.com>
2025-03-22 00:31:31 +0530

    Use OOP

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 import discord
-from discord.ext import commands
+from discord.mentions import AllowedMentions
 import env
 
-# Create a bot instance
 intents = discord.Intents.default()
-intents.messages = True  # Enable message intent
+intents.messages = True
 intents.message_content = True
 intents.dm_messages = True
 intents.guild_messages = True
 intents.guilds = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents.members = True
+allowed_mentions = AllowedMentions(users=True)
+
+class SwearAwayBot(discord.Client):
+	async def on_ready(self):
+		print(f'Logged in as {bot.user.name}')
+	
+	async def on_message(self, message: discord.Message):
+		# Prevent the bot from responding to its own messages
+		if message.author == bot.user:
+			return
+
+		if "fuck" in message.content.lower():
+			await message.delete()
+			await message.channel.send(prettify_message(message.author.name, message.content.replace("fuck", "flower")), allowed_mentions=allowed_mentions)
 
 def prettify_message(author_name: str, message: str):
-	return f"@{author_name}:\n{message}"
+	return f"@{author_name}:\n\"{message}\""
 
-@bot.event
-async def on_ready():
-	print(f'Logged in as {bot.user.name}')
-
-@bot.event
-async def on_message(message: discord.Message):
-	# Prevent the bot from responding to its own messages
-	if message.author == bot.user:
-		return
-
-	if "fuck" in message.content.lower():
-		await message.delete()
-		message.channel.send()
-		await message.channel.send(prettify_message(message.author.name, message.content.replace("fuck", "flower")))
-
-# Run the bot with your token
+bot = SwearAwayBot(intents=intents)
 bot.run(env.BOT_TOKEN)
